611eff3a5360c57c57de7023b89b6c5a9e9f90d1
gpciancio <gopalciancio@gmail.com>
2017-08-27 13:49:00 -0600

    api call sucess with return object

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      attribution: '',
+      html: true,
+      text: '',
+      ingredientLines: '',
+      recipe_ids: []
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleRecipe();
+  }
+
+  getSampleRecipe() {
+    axios.get('http://api.yummly.com/v1/api/recipes?_app_id=71956da8&_app_key=180ad96efd0415a982da2e0ab65c3fdd&q=&allowedIngredient[]=garlic&allowedIngredient[]=cognac')
+    .then( ( response ) => {
+      console.log( 'RESPONSE', response );
+      var matches = response.data.matches;
+      for( var index = 0; index < matches.length; index++ ) {
+        var recipeID = matches[index].id;
+        this.state.recipe_ids.push(recipeID);
+      }
+      for (var i = 0; i < recipe_ids.length; i++) {
+        axios.get(`http://api.yummly.com/v1/api/recipe/${recipe_ids[i]}?_app_id=71956da8&_app_key=180ad96efd0415a982da2e0ab65c3fdd`)
+        .then((response) => {
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+      // this.setState( { text: response.data.text }, function() {
+      //   console.log( 'STATE', this.state);
+      // });
+    })
+    .catch( ( err ) => {
+      console.log(err);
+    }
+  )
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      Hello World
       </div>
     );
   }
